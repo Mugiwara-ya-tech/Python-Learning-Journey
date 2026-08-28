1d1f5eb73af45e2339f281154d00c07fb446eb02
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-28 18:27:37 +0000

    initial commit

---
+++ variables.py
@@ -0,0 +1,13 @@
+# A variable name is used to identify where that information is stored.
+# You can access the value that a variable is storing by calling its name.
+# for example 
+
+name = "John"   # This is known as declaring a variable
+age = 19
+height = 180
+is_Student = True
+
+print(name)     # This is know as calling a variable
+print(age)
+print(height)
+print(is_Student)
