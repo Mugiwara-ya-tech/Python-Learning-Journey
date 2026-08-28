a8b32b67a72b9592ef4d0e9f049abb39bd7c5c74
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-28 18:41:14 +0000

    Basics

---
+++ comment.py
@@ -0,0 +1,18 @@
+# Comments are programmer-readable annotations in the source code that provide explanations or notes
+# making the code easier to understand and maintain.
+# These lines are completely ignored by the Python interpreter during execution
+# Meaning they have no effect on the programs output or performance
+
+# You can add comment a single line comment using '#' symbol
+# For multi line comment you can use ''' or """
+
+# Name is a comment here
+Name = input()
+
+''' Name   # here you dont have to add # symbol everytime in the new line
+is 
+a
+comment
+here
+'''
+Name = input()
