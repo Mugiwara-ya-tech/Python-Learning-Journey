1c4bf3d84957517945f7d7f5d22b4c5377d03749
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-29 19:30:45 +0000

    Basics

---
+++ data_types.py
@@ -0,0 +1,22 @@
+# String is the data type for a piece of text
+# The quotation marks tells the computer that a value needs to be stored as a string
+# Anything in quotation marks will be treated as a string even numbers
+
+name = "John"
+age = "42"
+
+# Integer are whole number without a decimal point
+# They can be positive, negative or zero
+
+age = 19
+
+# Float is the data type for numbers with decimal places, they can be positive or negative
+
+investment = 49.99
+
+# The + addition operator with string values the two strings are joined together
+# This is known as Concatenation
+
+a = "basket"
+b = "ball"
+print(a+b)
