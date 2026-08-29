04b29ab192d543448ddde54cdb4cb6a09c2631b1
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-29 19:37:09 +0000

    Basics

---
+++ data_type_checking.py
@@ -0,0 +1,11 @@
+# You can use the type() instruction to check the data type stored in a variable
+
+balance = "780"
+print(type(balance))
+
+city = "Berlin"
+age = 42
+balance = 830.29
+print(type(city))
+print(type(age))
+print(type(balance))
