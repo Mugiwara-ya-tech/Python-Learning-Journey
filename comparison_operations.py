0f8a58efecf4c8883b7e541edcea491ed8401614
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-30 11:34:21 +0530

    Delete comparison_operations.py

--- comparison_operations.py
+++
@@ -1,5 +0,0 @@
-# A comparison operation always result in either one of these two outcome: True or False
-
-50 > 100  # O/P is False
-
-# The Boolean has a data type that only has two possible values: True or False.
