922185ae7ef81f2c9f0457cc86382f9b5e58255d
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-30 11:34:00 +0530

    Add files via upload

---
+++ comparison_operations.py
@@ -0,0 +1,5 @@
+# A comparison operation always result in either one of these two outcome: True or False
+
+50 > 100  # O/P is False
+
+# The Boolean has a data type that only has two possible values: True or False.
