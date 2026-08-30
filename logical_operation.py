464d0964d2e72e80e19d67742f16924672fcd9d4
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-30 12:04:25 +0530

    Basics

---
+++ logical_operation.py
@@ -0,0 +1,18 @@
+# A logical operation takes several boolean inputs and produce only 1 boolean output.
+
+''' First one is the AND operation
+        True AND True = True
+        True AND False = False
+        False AND True = False
+        False AND False = False'''
+
+# The "AND" operation result in a True value only when all the inputs are True at the same time.
+# To memorize this remember this " True AND True = True, Every other combination = False".
+
+''' Second one is the OR operation
+        True OR True = True
+        True OR False = True
+        False OR True = True
+        False OR False = False'''
+
+# The "OR" operation result in a True value if at least one of the input is True.
