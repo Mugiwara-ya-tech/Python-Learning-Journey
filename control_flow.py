4b90102bc2b81ae52c3bd43f17c37c4ce505b95a
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-08-31 23:02:55 +0530

    Looping Concept

---
+++ control_flow.py
@@ -0,0 +1,7 @@
+# There are 3 technique: Sequencing, Iteration and Selection.
+
+# In sequencing the computer runs your code in order from top to bottom.
+
+# Iteration is about executing an instruction repeatedly. Its commonly represented as loop.
+
+# Selection specifies when to follow each path.
