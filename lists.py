55b440b93876b29758680abbb9565a3055574051
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-01 22:45:50 +0530

    Lists

---
+++ lists.py
@@ -0,0 +1,14 @@
+# Lists allow you to store a collection of multiple values in a single variable.
+# Add square brackets [] around the values to create a list
+
+cart = ["milk","tea","jam"]   # A list can store multiple values
+
+# The different items in a list need to be separated by commas.
+# List can store any data type, it can contain value of different data types.
+
+# List are ordered collections of items. You can access an item in a list using its position or index number
+
+animals = ["cat","dog","bird"]
+        #    0     1      2
+
+# The first item in a list has an index of 0.
