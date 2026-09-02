8aa8e3bf74b1f6daefa4e3eb5c965d1b99cb71df
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-02 23:11:13 +0530

    usage

---
+++ using_indexing.py
@@ -0,0 +1,22 @@
+name = "Lee"
+country = "France"
+user_info = [name, country]
+print(user_info[0]) # O/P is Lee
+
+products = ["juice","chocolate","water"]
+user_choice = 1
+print(products[user_choice]) # O/P is chocolate
+
+# Indexing also works with strings we can use indexing to access individual characters in a string
+
+''' DOG
+                D   O   G
+                0   1   2
+'''
+
+animal = "Dog"
+print(animal[0])  # O/P is D
+
+# The character in a string can include spaces & punctuation marks
+
+# Strings are immutable which means that you can't change the characters in a string. If you try to change a string you'll get an error.
