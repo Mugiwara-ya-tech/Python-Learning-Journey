c9ba5cc76b7b462f7f6ed03a5f698c59beb9ae7e
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-02 23:10:55 +0530

    indexing

---
+++ indexing.py
@@ -0,0 +1,20 @@
+animals = ["cat","dog","bird"]
+
+'''
+"cat" = animals[0]
+"dog" = animals[1]
+"bird" = animals[2]
+'''
+
+# We can use print() to display individual values on the screen
+
+last_calls = ["mom","Dave","Lawyer"]
+print(last_calls[0])   # O/P is mom
+
+# Lists are mutable, it means we can change their values even after they have been created
+
+nums = [8,6,19]
+nums[0] = 1
+nums[1] = 2
+nums[2] = 3
+print(nums)  # O/P is [1, 2, 3]
