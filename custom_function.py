42442e0870db01a643835b5f2030069b536932c4
Prateek Verma <124491068+Mugiwara-ya-tech@users.noreply.github.com>
2026-09-14 17:45:42 +0530

    user defined function 2

--- custom_function.py
+++ custom_function.py
@@ -1,39 +1,32 @@
-# To use your own function you need to define them first
-# Once a function has been defined you can call it as many times as you need
+# The result of a function can be sent back with the return statement.
+# This is particularly helpful when you need to continue using the result value in your program
 
-def greet():
-    print("Hello from a function")
-    print("Have a great day")
-greet()            # O/P is Hello from a function
-                   # O/P is Have a great day
-
-# Use def followed by a name to define a new function
-# The body of a function contain the reusable code that is executed when the function is called. The code for the body of a function must be indented
-# When a function is defined you need to make sure parenthesis() are added after the name. A colon(:) must be added at the end of the definition line
-
-# You can define function that takes any number of arguments(including zero). Argument are put inside the parenthesis() following the function name
-# A function might require argument to complete its task.
-
-def personal_greet(name):
-    print("Hello",name)
-    print("Have a great day")
-personal_greet("Sarah")
-personal_greet("Henry")
-
-'''
-O/P is 
-Hello Sarah
-Have a great day
-
-Hello Henry
-Have a great day
-'''
-
-# A function must be defined before they are called
-# A function can take as many argument as needed to complete the task
-
-# When calling a function you need to use the same number of argument that have been defined in the same order
 def bmi(weight,height):
     index = weight/(height*height)
-    print(index)
-bmi(56,180)
+    return index
+patient_5 = bmi(61,1.83)
+print("underweight:",patient_5 < 18.5)          # O/P is underweight: True
+
+patient_7 = bmi(75,1.74)
+print("underweight:",patient_7 < 18.5)          # O/P is underweight: False
+
+# A function can return multiple return value
+
+def rect(length,width):
+    area = length*width
+    perimeter = 2*length + 2*width
+    return area, perimeter
+
+x,y = rect(50,100)
+print(x,y)
+
+# Python allow function argument to have default values
+# If the functions is called without the argument the argument gets it default value
+
+def greet(name="Guest"):
+    print("Welcome",name)
+greet()              # O/P is Welcome Guest
+greet("John")        # O/P is Welcome John
+
+# Add the default value with the equal = sign to make the argument optional
+# The default value is used only if no other value has been passed as an argument when the function is called
